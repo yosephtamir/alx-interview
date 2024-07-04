14b9f29efdb5c5f935cede05f1f4f84d4b831f70
Yoseph Tamirat <yosephtamir@gmail.com>
2024-07-04 16:24:15 +0300

    Python Lockboxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -26,14 +26,3 @@ def canUnlockAll(boxes):
     if len(keys) == length:
         return True
     return False
-
-
-
-boxes = [[1], [2], [3], [4],[]]
-print(canUnlockAll(boxes))
-
-boxes = [[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]]
-print(canUnlockAll(boxes))
-
-boxes = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
-print(canUnlockAll(boxes))
